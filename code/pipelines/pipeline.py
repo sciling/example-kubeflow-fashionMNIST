0a0721621115d64620862feb53d9a5232529dfc2
Vicent Alabau <valabau@sciling.com>
2021-04-29 18:50:02 +0200

    Fix pipeline generation

--- code/pipelines/pipeline.py
+++ code/pipelines/pipeline.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '../src'))
+
+
 import kfp
 import kfp.dsl as dsl
 import kfp.components as comp
@@ -5,31 +11,30 @@ import kfp.components as comp
 
 # Define the pipeline
 @dsl.pipeline(
-   name='MNIST Pipeline',
-   description='A toy pipeline that performs mnist model training and prediction.'
+    name='MNIST Pipeline',
+    description='A toy pipeline that performs mnist model training and prediction.'
 )
-
 # Define parameters to be fed into pipeline
 def mnist_container_pipeline(
     compile_optimizer: str = "adam",
     epochs: int = 50,
     validation_split: float = 0.15,
 ):
-    from train import train
-    from test import test
-    from confusion_matrix import confusion_matrix
+    from models.train_model import train
+    from models.predict_model import test
+    from visualization.confusion_matrix import confusion_matrix
     # Create train and predict lightweight components.
     train_op = comp.func_to_container_op(train, base_image='tensorflow/tensorflow:latest-gpu-py3', packages_to_install=['matplotlib'])
     test_op = comp.func_to_container_op(test, base_image='tensorflow/tensorflow:latest-gpu-py3', packages_to_install=['scikit-learn'])
     confusion_matrix_op = comp.func_to_container_op(confusion_matrix, packages_to_install=['scikit-learn'])
-    
+
     # Create MNIST training component.
     mnist_training_container = train_op(compile_optimizer, epochs, validation_split)
     mnist_training_container.execution_options.caching_strategy.max_cache_staleness = "P0D"
 
     # Create MNIST prediction component.
     mnist_predict_container = test_op(mnist_training_container.outputs['data'])
-    
+
     confusion_matrix_op(mnist_predict_container.outputs['labels_dir'])
 
 
